fe0c2b7ebbb4a00fb8d4b232a0e5ead0b7ae31dd
Valerie Manuel <valerie.manuel11@gmail.com>
2019-07-18 18:45:53 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,14 +1,14 @@
 class Artist 
   attr_accessor :name 
   
-  @@all = []
+  # @@all = []
   
   def initialize(name)
     @name = name
     ##@@all << self
   end
   
-  def self.all
-    @@all
-  end
+  # def self.all
+  #   @@all
+  # end
 end
